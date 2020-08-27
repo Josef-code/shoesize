0bf3080e479f0ba79ff3e57fe056299751653f25
Joseph Bassey <jbbliss9@gmail.com>
2020-08-27 18:17:43 +0100

    added new folder and css

---
+++ index.php
@@ -0,0 +1,34 @@
+<body>
+    <?php require 'header.php' ?>
+    <div class="container  headholder">
+            <h2>Shoe Size Conversion Calculator. You can generate the perfect shoe size for your foot</h2>
+        <div>
+            <div class="dbsection">
+                <div class="formarea">
+                <?php echo "I'm a form area"; ?>
+                </div>
+                <div class="helparea">
+                     <?php echo "I'm a help area"; ?>
+                </div>
+            <!-- </div>
+                    <form action="results.php" method="POST">
+                        <label for="name"><h3>Your name:</h3></label>
+                        <input type="text" id="username" name="name" placeholder="John Kanu">
+
+                        <label for="gender" name="gender"><h3>Gender</h3></label>
+                        <select id="gender" name="gender">
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                        </select>
+
+                        <label for="length" name="length"><h3>Length of Foot</h3></label>
+                        <input type="text" id="length" name="length" placeholder="Input your shoe length">
+                        
+
+                        <input type="submit" value="submit">
+                    </form> -->
+        </div>
+        <hr/>
+    </div>
+</body>
+</html>
