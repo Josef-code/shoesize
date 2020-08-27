bf49719700f661e3fb4c320d99856e0ba25cfb1a
Joseph Bassey <jbbliss9@gmail.com>
2020-08-27 19:23:09 +0100

    added jquery functionality but it is not woring on the forms

--- index.php
+++ index.php
@@ -5,30 +5,41 @@
         <div>
             <div class="dbsection">
                 <div class="formarea">
-                <?php echo "I'm a form area"; ?>
+                    GET STARTED
                 </div>
                 <div class="helparea">
-                     <?php echo "I'm a help area"; ?>
+                     <a href="#">MEASUREMENT GUIDE</a>
                 </div>
-            <!-- </div>
-                    <form action="results.php" method="POST">
-                        <label for="name"><h3>Your name:</h3></label>
-                        <input type="text" id="username" name="name" placeholder="John Kanu">
+            </div>
 
-                        <label for="gender" name="gender"><h3>Gender</h3></label>
-                        <select id="gender" name="gender">
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
-                        </select>
-
-                        <label for="length" name="length"><h3>Length of Foot</h3></label>
-                        <input type="text" id="length" name="length" placeholder="Input your shoe length">
-                        
-
-                        <input type="submit" value="submit">
-                    </form> -->
         </div>
-        <hr/>
+            <hr/>
+        <div class="content">
+            loading...
+        </div>
     </div>
+        <script>
+            $(".formarea").click(function(){
+               $(".content").html("");
+            })
+        </script>
+
+                        <form action="results.php" method="POST">
+                            <label for="name"><h3>Your name:</h3></label>
+                            <input type="text" id="username" name="name" placeholder="John Kanu"\>
+
+                            <label for="gender" name="gender"><h3>Gender</h3></label>
+                            <select id="gender" name="gender">
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                            </select>
+
+                            <label for="length" name="length"><h3>Length of Foot</h3></label>
+                            <input type="text" id="length" name="length" placeholder="Input your shoe length">
+                            
+
+                            <input type="submit" value="submit">
+                        </form>
 </body>
 </html>
+                        
