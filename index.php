bb4c7e94bb72ffd2c6969e1cd9bd8137b69198b5
Joseph Bassey <jbbliss9@gmail.com>
2020-08-31 23:33:33 +0100

    worked on  the changing elements using jquery

--- index.php
+++ index.php
@@ -8,17 +8,15 @@
                     GET STARTED
                 </div>
                 <div class="helparea">
-                     <a href="#">MEASUREMENT GUIDE</a>
+                     MEASUREMENT GUIDE
                 </div>
             </div>
 
         </div>
             <hr/>
-        <div class="content">
+        <div id="content">
+            <div class="form">
                         <form action="results.php" method="POST">
-                            <label for="name"><h3>Your name:</h3></label>
-                            <input type="text" id="username" name="name" placeholder="John Kanu"\>
-
                             <label for="gender" name="gender"><h3>Gender</h3></label>
                             <select id="gender" name="gender">
                                 <option value="male">Male</option>
@@ -30,16 +28,30 @@
                             
 
                             <input type="submit" value="submit">
-                        </form>
+                        </form>  
+            </div>
+            <div class="help">
+                <h2>How to measure</h2>
+                <p>To measure your feet stand on a level floor with the back of your heels against a straight edge or a wall</p>
+            </div>
         </div>
     </div>
         <script>
+            $(".form").hide();
             $(".formarea").click(function(){
-               $(".content").hide();
+                // $("#content").show();
+                $(".form").show();
+                $(".help").hide();
+            })
+            $(".help").hide();
+            $(".helparea").click(function(){
+                // $("#content").show();
+                $(".help").show();
+                $(".form").hide();
             })
         </script>
 
 
 </body>
 </html>
-                        
+                       
