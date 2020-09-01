1d2fbb56251542e58c0676d4c4496eedb4e6451c
Joseph Bassey <jbbliss9@gmail.com>
2020-09-01 19:01:23 +0100

    everything is working well so far lol

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
             <hr/>
         <div id="content">
             <div class="form">
-                        <form action="../shoesize/private/results.php" method="POST">
+                        <form action="results.php" method="POST">
                             <label for="gender" name="gender"><h3>Gender</h3></label>
                             <select id="gender" name="gender">
                                 <option value="male">Male</option>
@@ -32,7 +32,13 @@
             </div>
             <div class="help">
                 <h2>How to measure</h2>
-                <p>To measure your feet stand on a level floor with the back of your heels against a straight edge or a wall</p>
+                <ol id="desc">
+                    <li>To measure your feet stand on a level floor with the back of your heels against a straight edge or a wall</li>
+                    <li>Remember to measure both feet, making a note of the longest feet</li>
+                    <li>Measure your foot length by placing a ruler flat on the floor straight alongside the inside of your foot from your heal to your toes</li>
+                    <li>Place an object with a flat edge straight across your toes with the edge touching the tip of your longest toe. Take measurement ( in centimeters ) from the ruler where the flat edge crosses. This is your foot measurement.</li>
+                    <li>If your foot measurement is halfway between sizez, select the larger size.</li>
+                </ol>
             </div>
         </div>
     </div>
---
+++ results.php
@@ -0,0 +1,6 @@
+<?php 
+    $name = $_POST['length'];
+    if($name == 'okayjdjjd'){
+        echo "not again !";
+    }
+?>
